f4c4bd216ebec8a91e85cfca66acff01ee9f8a62
k4u5h4L <kaushal.v.bhat@gmail.com>
2021-12-05 16:45:46 +0530

    finish discover page

--- lib/view/screens/discover_page.dart
+++ lib/view/screens/discover_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:thebrocolli/model/core/news.dart';
 import 'package:thebrocolli/model/core/video_news.dart';
 import 'package:thebrocolli/model/helper/news_helper.dart';
@@ -16,16 +19,21 @@ class DiscoverPage extends StatefulWidget {
   _DiscoverPageState createState() => _DiscoverPageState();
 }
 
-class _DiscoverPageState extends State<DiscoverPage> with TickerProviderStateMixin {
+class _DiscoverPageState extends State<DiscoverPage>
+    with TickerProviderStateMixin {
   ScrollController _featuredVideoNewsCardScrollController = ScrollController();
   TabController _categoryTabController;
   List<VideoNews> featuredVideoNews = VideoNewsHelper.featuredVideoNews;
   List<News> allCategoriesNews = NewsHelper.allCategoriesNews;
+  CollectionReference news = FirebaseFirestore.instance.collection('news');
+
+  List<String> categories = ['All categories', 'Covid', 'Software', 'India'];
 
   @override
   void initState() {
     super.initState();
-    _categoryTabController = TabController(length: 7, vsync: this);
+    _categoryTabController =
+        TabController(length: categories.length, vsync: this);
   }
 
   _changeTab(index) {
@@ -61,7 +69,8 @@ class _DiscoverPageState extends State<DiscoverPage> with TickerProviderStateMix
         actions: [
           IconButton(
             onPressed: () {
-              Navigator.of(context).push(SlidePageRoute(child: SearchPage(), direction: AxisDirection.up));
+              Navigator.of(context).push(SlidePageRoute(
+                  child: SearchPage(), direction: AxisDirection.up));
             },
             icon: SvgPicture.asset(
               'assets/icons/Search.svg',
@@ -75,61 +84,66 @@ class _DiscoverPageState extends State<DiscoverPage> with TickerProviderStateMix
         physics: BouncingScrollPhysics(),
         children: [
           // Section 1 - Featured News Video
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 16),
-            width: MediaQuery.of(context).size.width,
-            height: 245,
-            color: Colors.black,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(bottom: 20),
-                  height: 170,
-                  child: ListView.separated(
-                    controller: _featuredVideoNewsCardScrollController,
-                    padding: EdgeInsets.only(left: 16),
-                    scrollDirection: Axis.horizontal,
-                    physics: BouncingScrollPhysics(),
-                    itemCount: featuredVideoNews.length,
-                    separatorBuilder: (context, index) {
-                      return SizedBox(
-                        width: 10,
-                      );
-                    },
-                    itemBuilder: (context, index) {
-                      return FeaturedVideoNewsCard(
-                        data: featuredVideoNews[index],
-                      );
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Video News',
-                        style: TextStyle(
-                          color: Colors.white.withOpacity(0.6),
-                        ),
-                      ),
-                      ScrollIndicator(
-                        scrollController: _featuredVideoNewsCardScrollController,
-                        height: 6,
-                        width: 30,
-                        indicatorWidth: 20,
-                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(10), color: Colors.white.withOpacity(0.3)),
-                        indicatorDecoration: BoxDecoration(color: Colors.white.withOpacity(0.2), borderRadius: BorderRadius.circular(10)),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
+          // Container(
+          //   padding: EdgeInsets.symmetric(vertical: 16),
+          //   width: MediaQuery.of(context).size.width,
+          //   height: 245,
+          //   color: Colors.black,
+          //   child: Column(
+          //     mainAxisAlignment: MainAxisAlignment.start,
+          //     crossAxisAlignment: CrossAxisAlignment.start,
+          //     children: [
+          //       Container(
+          //         margin: EdgeInsets.only(bottom: 20),
+          //         height: 170,
+          //         child: ListView.separated(
+          //           controller: _featuredVideoNewsCardScrollController,
+          //           padding: EdgeInsets.only(left: 16),
+          //           scrollDirection: Axis.horizontal,
+          //           physics: BouncingScrollPhysics(),
+          //           itemCount: featuredVideoNews.length,
+          //           separatorBuilder: (context, index) {
+          //             return SizedBox(
+          //               width: 10,
+          //             );
+          //           },
+          //           itemBuilder: (context, index) {
+          //             return FeaturedVideoNewsCard(
+          //               data: featuredVideoNews[index],
+          //             );
+          //           },
+          //         ),
+          //       ),
+          //       Padding(
+          //         padding: EdgeInsets.symmetric(horizontal: 16),
+          //         child: Row(
+          //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //           children: [
+          //             Text(
+          //               'Video News',
+          //               style: TextStyle(
+          //                 color: Colors.white.withOpacity(0.6),
+          //               ),
+          //             ),
+          //             ScrollIndicator(
+          //               scrollController:
+          //                   _featuredVideoNewsCardScrollController,
+          //               height: 6,
+          //               width: 30,
+          //               indicatorWidth: 20,
+          //               decoration: BoxDecoration(
+          //                   borderRadius: BorderRadius.circular(10),
+          //                   color: Colors.white.withOpacity(0.3)),
+          //               indicatorDecoration: BoxDecoration(
+          //                   color: Colors.white.withOpacity(0.2),
+          //                   borderRadius: BorderRadius.circular(10)),
+          //             ),
+          //           ],
+          //         ),
+          //       )
+          //     ],
+          //   ),
+          // ),
 
           // Section 2 - News Based on Category
           Container(
@@ -144,75 +158,28 @@ class _DiscoverPageState extends State<DiscoverPage> with TickerProviderStateMix
                   child: TabBar(
                     isScrollable: true,
                     controller: _categoryTabController,
-                    labelStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, fontFamily: 'inter'),
+                    labelStyle: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: 'inter'),
                     labelColor: Colors.black,
-                    unselectedLabelStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.w400, fontFamily: 'inter'),
+                    unselectedLabelStyle: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'inter'),
                     unselectedLabelColor: Colors.black.withOpacity(0.6),
                     indicatorColor: Colors.transparent,
                     onTap: _changeTab,
-                    tabs: [
-                      Tab(
-                        text: 'All categories',
-                      ),
-                      Tab(
-                        text: 'Covid19',
-                      ),
-                      Tab(
-                        text: 'International',
-                      ),
-                      Tab(
-                        text: 'Europe',
-                      ),
-                      Tab(
-                        text: 'American',
-                      ),
-                      Tab(
-                        text: 'Asian',
-                      ),
-                      Tab(
-                        text: 'Sports',
-                      ),
-                    ],
+                    tabs: categories
+                        .map(
+                          (name) => Tab(text: name),
+                        )
+                        .toList(),
                   ),
                 ),
                 IndexedStack(
                   index: _categoryTabController.index,
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      child: ListView.separated(
-                        padding: EdgeInsets.symmetric(horizontal: 16),
-                        scrollDirection: Axis.vertical,
-                        physics: NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: allCategoriesNews.length,
-                        separatorBuilder: (context, index) {
-                          return SizedBox(height: 16);
-                        },
-                        itemBuilder: (context, index) {
-                          return NewsTile(data: allCategoriesNews[index]);
-                        },
-                      ),
-                    ),
-                    SizedBox(
-                      child: Center(child: Text('category page ${_categoryTabController.index}')),
-                    ),
-                    SizedBox(
-                      child: Center(child: Text('category page ${_categoryTabController.index}')),
-                    ),
-                    SizedBox(
-                      child: Center(child: Text('category page ${_categoryTabController.index}')),
-                    ),
-                    SizedBox(
-                      child: Center(child: Text('category page ${_categoryTabController.index}')),
-                    ),
-                    SizedBox(
-                      child: Center(child: Text('category page ${_categoryTabController.index}')),
-                    ),
-                    SizedBox(
-                      child: Center(child: Text('category page ${_categoryTabController.index}')),
-                    ),
-                  ],
+                  children: getCategoriesWidgets(),
                 ),
               ],
             ),
@@ -220,5 +187,104 @@ class _DiscoverPageState extends State<DiscoverPage> with TickerProviderStateMix
         ],
       ),
     );
+  }
+
+  List<Widget> getCategoriesWidgets() {
+    List<Widget> list = [
+      Container(
+        width: MediaQuery.of(context).size.width,
+        child: FutureBuilder<QuerySnapshot>(
+            future: news.get(),
+            builder:
+                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                );
+              }
+
+              List<News> data = [];
+              snapshot.data.docs.forEach((a) => data.add(
+                    (News(
+                        title: a['title'],
+                        description: a['description'],
+                        photo: a['photo'],
+                        author: a['author'],
+                        date: a['date'],
+                        trending: a['trending'])),
+                  ));
+
+              return ListView.separated(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                scrollDirection: Axis.vertical,
+                physics: NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: data.length,
+                separatorBuilder: (context, index) {
+                  return SizedBox(height: 16);
+                },
+                itemBuilder: (context, index) {
+                  return NewsTile(data: data[index]);
+                },
+              );
+            }),
+      ),
+    ];
+
+    for (var i = 1; i < categories.length; i++) {
+      list.add(
+        Container(
+          width: MediaQuery.of(context).size.width,
+          child: FutureBuilder<QuerySnapshot>(
+              future: news.get(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.black,
+                    ),
+                  );
+                }
+
+                List<News> data = [];
+                snapshot.data.docs.forEach((a) => {
+                      // if (a['title'])
+                      if (a['title'].toLowerCase().contains(
+                          categories[_categoryTabController.index]
+                              .toLowerCase()))
+                        {
+                          data.add(
+                            (News(
+                                title: a['title'],
+                                description: a['description'],
+                                photo: a['photo'],
+                                author: a['author'],
+                                date: a['date'],
+                                trending: a['trending'])),
+                          )
+                        }
+                    });
+
+                return ListView.separated(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  scrollDirection: Axis.vertical,
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: data.length,
+                  separatorBuilder: (context, index) {
+                    return SizedBox(height: 16);
+                  },
+                  itemBuilder: (context, index) {
+                    return NewsTile(data: data[index]);
+                  },
+                );
+              }),
+        ),
+      );
+    }
+    return list;
   }
 }
